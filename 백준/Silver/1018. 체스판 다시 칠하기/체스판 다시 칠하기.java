22747f30cc5ad02e05321391811f8bc4e5f2d5c8
미밍 <133966179+mjkang4416@users.noreply.github.com>
2025-09-26 16:22:08 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 212 ms, Memory: 41308 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        char[][] arr = new char[N][M];
+        int result = 999999999;
+
+        for(int i =0; i<N; i++){
+            String line = br.readLine();
+            for(int j=0; j<M; j++){
+                arr[i][j] = line.charAt(j);
+            }
+        }
+
+        for(int i =0; i<=N-8; i++) { //완전탐색
+            for (int j = 0; j <=M-8; j++) { //하나씩 내리면서 가로로 탐색
+                int wResult = 0;
+                int bResult = 0;
+                int count = 0;
+
+                //첫칸이 하얀색일 경우 계산
+                    char[][] tempArr1 = arr.clone();
+                    for(int k = 0; k < N; k++) {
+                        tempArr1[k] = arr[k].clone();
+                    }
+                    if(tempArr1[i][j] != 'W'){
+                        tempArr1[i][j] = 'W';
+                        wResult++;
+                    }
+                    wResult += test(tempArr1, i, j);
+
+                //첫칸이 검은색일 경우 계산
+                    char[][] tempArr2 = arr.clone();
+                    for(int k = 0; k < N; k++) {
+                        tempArr2[k] = arr[k].clone();
+                    }
+                    if(tempArr2[i][j] != 'B'){
+                        tempArr2[i][j] = 'B';
+                        bResult++;
+                    }
+                    bResult += test(tempArr2, i, j);
+
+                count = Math.min(wResult, bResult);
+                result = Math.min(result,count);
+            }
+        }
+        System.out.println(result);
+    }
+
+    public static int test(char[][] arr, int i, int j) {
+        int count = 0;
+        for (int x = 0; x < 8; x++) { //8*8 전체탐색
+            for (int y = 0; y < 8; y++) {
+                if (x <7 && arr[i + x][j] == arr[i + x + 1][j]) { //첫줄과 다음줄의 첫문자가 같은경우
+                    count++;
+                    if (arr[i + x][j] == 'W') {
+                        arr[i + x + 1][j] = 'B';
+                    } else {
+                        arr[i + x + 1][j] = 'W';
+                    }
+                }
+                if (y<7 && arr[i + x][j + y] == arr[i + x][j + y + 1]) { //이번 문자와 다움 문자가 같은 경우
+                    count++;
+                    if (arr[i + x][j + y] == 'W') {
+                        arr[i + x][j + y + 1] = 'B';
+                    } else {
+                        arr[i + x][j + y + 1] = 'W';
+                    }
+
+                }
+            }
+        }
+        return count;
+    }
+}
