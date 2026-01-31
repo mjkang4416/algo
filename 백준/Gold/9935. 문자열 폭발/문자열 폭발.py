8c003c1c32f9fff1908ed2cc86c837b84c672382
강민정 <133966179+mjkang4416@users.noreply.github.com>
2026-02-01 00:20:30 +0900

    [Gold IV] Title: 문자열 폭발, Time: 616 ms, Memory: 44620 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,37 +1,22 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-st = input().rstrip()
-bumm = input().rstrip()
+origin_str = input().rstrip()
+pop_str = list(input().rstrip())
+
+n = len(origin_str)
+pn = len(pop_str)
 stack = []
-n = len(bumm)
 
-for i in st:
-    if len(stack)>=n and stack[-1] == bumm[-1]:
-        is_true = True 
-        for j in range(n): #4 개 검사했는데 다른게 있으면 계속 진행, 아니면 pop
-            if stack[len(stack)-j-1] != bumm[n-j-1]:
-                is_true = False #다른게 있는경우 
-                break
-        if is_true :
-            for _ in range(n):
-                stack.pop()
-
-
-    stack.append(i)       
-
-if len(stack)>=n and stack[-1] == bumm[-1]:
-    is_true = True 
-    for j in range(n): #4 개 검사했는데 다른게 있으면 계속 진행, 아니면 pop
-        if stack[len(stack)-j-1] != bumm[n-j-1]:
-            is_true = False #다른게 있는경우 
-            break
-    if is_true :
-        for _ in range(n):
+for i in range(n):
+    stack.append(origin_str[i])
+  
+    if stack[len(stack)-pn:len(stack)] == pop_str:
+         for _ in range(pn):
             stack.pop()
 
-if len(stack):
-    for i in stack:
-        print(i,end="")
+if stack:
+    print(''.join(stack))
 else:
     print("FRULA")
