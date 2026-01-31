73b598d09c817defaac4d4476477e7924aa4a8be
강민정 <133966179+mjkang4416@users.noreply.github.com>
2026-02-01 02:24:55 +0900

    [Gold V] Title: 치킨 배달, Time: 280 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,25 +1,47 @@
 import sys
-import itertools
+from collections import deque
 input = sys.stdin.readline
-
-n,m =map(int,input().split())
+#중복있게 n 중  m개를 고른 수열 -> 중복조합
+n,m = map(int,input().split())
 arr = [list(map(int,input().split())) for _ in range(n)]
-chicken = []
-room = []
+#2를 m 개 고를때 1 과 2의 거리의 최솟값
+chicken_arr = []
+houses = []
+answer = float('inf')
 for i in range(n):
     for j in range(n):
         if arr[i][j] == 2:
-            chicken.append((i,j))
-        elif arr[i][j] == 1:
-            room.append((i,j))
-result_chicken = list(itertools.combinations(chicken,m))
-result = 2e9
-for i in result_chicken: #m개 조합
-    min_dist = 0 #m 개 합
-    for j in room: #하나집과 치킨집 m 개중 젤 작은 거리
-        temp = 2e9
-        for k in i:
-            temp = min(temp,abs(j[0]-k[0])+abs(j[1]-k[1])) #해당 조합 내의 치킨집중 가장 거리 작은거
-        min_dist+=temp
-    result= min(result,min_dist)
-print(result)
+            chicken_arr.append((i,j))
+        if arr[i][j] ==1:
+            houses.append((i,j))
+
+
+def combination(cnt,start,minus_arr):
+    if cnt == m:
+        sum_chicken_dist(list(minus_arr)) #치킨집 조합
+        return
+
+    for i in range(start,len(chicken_arr)):
+        temp = chicken_arr[i]
+        minus_arr.append((temp[0],temp[1]))
+        combination(cnt+1,i+1,minus_arr)
+        minus_arr.pop()
+
+def sum_chicken_dist(minus_arr):
+    result = 0
+    global answer
+    for house in houses:
+        dist = float('inf')
+        for chicken_house in minus_arr: #조합별 집들과의 거리
+            house_x = house[0]
+            house_y = house[1]
+            chicken_house_x = chicken_house[0]
+            chicken_house_y = chicken_house[1]
+            dist = min(dist,abs(house_x-chicken_house_x)+abs(house_y-chicken_house_y))
+        result+=dist
+
+    answer = min(answer,result)
+
+combination(0,0,[])
+
+print(answer)
