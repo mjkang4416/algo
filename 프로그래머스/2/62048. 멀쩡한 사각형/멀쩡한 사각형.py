03c4508d68086d5887008a9857348b66212f5eb7
강민정 <133966179+mjkang4416@users.noreply.github.com>
2026-02-01 12:13:17 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.00 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
@@ -1,9 +1,3 @@
-import math
-
 def solution(w,h):
-    if w==h :
-        return w*h-w
-    else:
-        temp = math.gcd(w,h)
-        return w*h-(w+h-temp) 
-    
+    answer = 1
+    return answer
